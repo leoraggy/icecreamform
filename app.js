9325ac5e05da864fa4be71b99cd0e0a87748f3be
leoraggy <leoragual35@gmail.com>
2025-10-28 11:59:06 -0700

    Started ice cream routing and ejs

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 const PORT = 3001;
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+
+const orders = [];
+
 app.get("/", (req, res) => {
-  res.sendFile(`${import.meta.dirname}/views/home.html`);
+  res.render("home");
 });
+
+app.get("/confirm", (req, res) => {
+  res.render('confirm');
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
