474e7cf9e574b79746bdf022e1a94f66656e8f0a
leoraggy <leoragual35@gmail.com>
2025-11-03 09:47:54 -0800

    Added check for if toppings is empty

--- app.js
+++ app.js
@@ -13,25 +13,30 @@ app.use(express.urlencoded({ extended: true }));
 const orders = [];
 
 app.get("/", (req, res) => {
-	res.render("home");
+  res.render("home");
 });
 
 app.post("/submit-order", (req, res) => {
-	const order = req.body;
-	order.time = new Date();
-	orders.push(order);
-	res.render("confirm", { order });
-	// res.json(order);
+  const order = req.body;
+  if (!order.toppings) {
+    order.toppings = ["No toppings"];
+  }
+  order.time = new Date();
+
+  orders.push(order);
+
+  res.render("confirm", { order });
+  // res.json(order);
 });
 
 app.get("/confirm", (req, res) => {
-	res.render("confirm");
+  res.render("confirm");
 });
 
 app.get("/admin", (req, res) => {
-	res.render("admin", { orders });
+  res.render("admin", { orders });
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
